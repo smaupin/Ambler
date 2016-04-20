f92d6782e0b830e0cafab278bb15d984451e1673
noel-arce <n.arcellana.00@gmail.com>
2016-04-20 15:03:57 -0700

    plotted 2nd pin(test)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,46 +1,61 @@
-angular.module('ambler.controllers', [])
+angular.module('ambler.controllers', [])  
 
 .controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
-	var options = {timeout: 10000, enableHighAccuracy: true};
+	var options = {enableHighAccuracy: true}; //timeout: 10000,
 	 
+  //GEOLOCATION
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
     
-    //THIS SETS LAT/LONG GEOLOCATION
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    //LAT/LONG FOR GEOLOCATION & WOTHER PINS
+    var userLoc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        ferryLoc = new google.maps.LatLng(37.795800, -122.393459);
  
     var mapOptions = {
-      center: latLng,
-      zoom: 15,
+      center: userLoc,
+      zoom: 14,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
  
-    //INPUTS MAP WITH MAP OPTIONS INTO DIV.ID MAP
+    //INPUTS MAP WITH "mapOtions" SETTINGS INTO #ID DIV IN HTML
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    //WAIT UNTIL MAP IS LOADED
     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-     
-      var marker = new google.maps.Marker({
-          map: $scope.map,
-          animation: google.maps.Animation.DROP,
-          position: latLng
+      
+      //DROPS PIN ON CURRENT LOCATxION
+      var userPin = new google.maps.Marker({
+        map: $scope.map,
+        animation: google.maps.Animation.DROP,
+        position: userLoc
       });      
      
-      var infoWindow = new google.maps.InfoWindow({
-          content: "Here I am!"
+      var userWindow = new google.maps.InfoWindow({
+        content: "Here I am!"
+      });
+
+      //DROPS PIN ON 2ND+ LOCATION
+      var ferryPin = new google.maps.Marker({
+        map: $scope.map,
+        animation: google.maps.Animation.DROP,
+        position: ferryLoc
+      });
+
+      var ferryWindow = new google.maps.InfoWindow({
+        content: "Ferry Building"
       });
      
-      google.maps.event.addListener(marker, 'click', function () {
-          infoWindow.open($scope.map, marker);
+      //CLICK LISTENERS FOR PIN INFO WINDOWS
+      google.maps.event.addListener(userPin, 'click', function () {
+        userWindow.open($scope.map, userPin);
+      });
+
+      google.maps.event.addListener(ferryPin, 'click', function () {
+        ferryWindow.open($scope.map, ferryPin);
       });
 
     });
-
-
  
   }, function(error){
     console.log("Could not get location");
   });
-
 
 });
